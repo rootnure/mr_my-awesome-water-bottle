737f46a947c8aee5a99f54a31b5bedd209917f6d
rootnure <nur.diu.2791@gmail.com>
2026-03-13 23:03:41 +0600

    Buy Now Button Click Handler Function added

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Bottle.css";
 
-const Bottle = ({ bottle }) => {
-    const { img } = bottle;
+const Bottle = ({ bottle, handleAddToCart }) => {
+    const { img, name, price, stock } = bottle;
     return (
-        <div className="card">
+        <div className="card bottle">
             <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p>${price}</p>
+            <p>Available: {stock}</p>
+            <button onClick={() => handleAddToCart(bottle)} className="btn">
+                Buy Now
+            </button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,16 +1,27 @@
-import { use } from "react";
+import { use, useState } from "react";
 import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
 
 const Bottles = ({ bottlesPromise }) => {
+    const [cart, setCart] = useState([]);
+
     const bottles = use(bottlesPromise);
+
+    const handleAddToCart = (bottle) => {
+        console.log(bottle);
+    };
     return (
         <div>
             <h1>My Awesome Water Bottles</h1>
             <h3>Bottles: {bottles.length}</h3>
-            <div>
+            <div className="bottles-container">
                 {bottles &&
                     bottles.map((bottle) => (
-                        <Bottle key={bottle.id} bottle={bottle} />
+                        <Bottle
+                            key={bottle.id}
+                            bottle={bottle}
+                            handleAddToCart={handleAddToCart}
+                        />
                     ))}
             </div>
         </div>
